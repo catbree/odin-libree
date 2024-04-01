9fd44bb4a3ddd1b75f5e2b42666c775c497948a3
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-04-01 08:06:29 +0800

    refactored library to use classes instead of constructor

--- script.js
+++ script.js
@@ -10,69 +10,98 @@ class Book {
     toggleStatus() {
         if (this.status == true) {
             this.status = false;
-            console.log(`status is read, toggling to ${this.status}`);
         }
         else if (this.status == false) {
             this.status = true;
-            console.log(`status is not read, toggling to ${this.status}`);
         }
     }
 }
 
-let library = [];
+class Library {
+    constructor() {
+        this.books = []
+    }
 
+    addBook(newBook) {
+        this.books.push(newBook);
+    }
 
-library[0] = new Book("Daring Greatly", "Brene Brown", 320, true, 0);
-library[1] = new Book("Bridge to Terabithia", "Katherine Paterson", 208, false, 1);
-library[2] = new Book("Animal Farm", "Goerge Orwell", 176, false, 2);
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title);
+    }
 
-const bookList = document.querySelector(".bookList");
-updateBookDisplay(library);
-
-function refreshBookDisplay() {
-    bookList.textContent="";
+    getBook(title) {
+        return this.books.find((book) => book.title == title)
+    }
 }
 
-function updateBookDisplay(library) {
-    
-    for(i=0;i<library.length;i++) {
-        const book = document.createElement("div");
-        book.classList.add("book");
+const displayController = (function() {
 
+    const bookList = document.querySelector(".bookList");
+
+    const updateBooksDisplay = () => {
+        refreshBooksDisplay();
+        for (let book of library.books) {
+            createBookCard(book);
+        }
+    }
+
+    const refreshBooksDisplay = () => {
+        bookList.textContent="";
+    }
+
+    const createBookCard = (book) => {
+        const bookCard = document.createElement("div");
         const bookContent = document.createElement("div");
-        book.classList.add("bookContent");
-
         const bookTitle = document.createElement("h2");
-        bookTitle.textContent = library[i].title;
-        bookTitle.classList.add("bookTitle");
-
         const bookAuthor = document.createElement("p");
-        bookAuthor.textContent = library[i].author;
-        bookAuthor.classList.add("bookAuthor");
-
         const bookPages = document.createElement("p");
-        bookPages.textContent = library[i].pages;
-        bookPages.classList.add("bookPages");
-
         const bookStatus = document.createElement("p");
-        if (library[i].status == true) {
+        const buttonGroup = document.createElement("div");
+        const markAsReadButton = document.createElement("button");
+        const deleteBookButton = document.createElement("button");
+        
+        bookCard.classList.add("book");
+        bookCard.classList.add("bookContent");
+        bookTitle.classList.add("bookTitle");
+        bookAuthor.classList.add("bookAuthor");
+        bookPages.classList.add("bookPages");
+        bookStatus.classList.add("bookStatus");
+        buttonGroup.classList.add("buttonGroup");
+        markAsReadButton.classList.add("markAsReadButton");
+        deleteBookButton.classList.add("redButton");
+        deleteBookButton.classList.add("deleteBookButton");
+
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages;
+        markAsReadButton.textContent = "Toggle Status";
+        deleteBookButton.textContent = "Delete";
+        
+        if (book.status == true) {
             bookStatus.textContent = "Read";
         } else {
             bookStatus.textContent = "Not Read";
         }
-        bookStatus.classList.add("bookStatus");
 
-        const buttonGroup = document.createElement("div");
-        buttonGroup.classList.add("buttonGroup");
+        deleteBookButton.addEventListener("click", (e) => {
+            const title = e.target.parentNode.parentNode.querySelector(".bookTitle").textContent;
+            library.removeBook(title);
+            updateBooksDisplay();
+        })
 
-        const markAsReadButton = document.createElement("button");
-        markAsReadButton.textContent = "Toggle Status";
-        markAsReadButton.classList.add("markAsReadButton");
-
-        const deleteBookButton = document.createElement("button");
-        deleteBookButton.textContent = "Delete";
-        deleteBookButton.classList.add("redButton");
-        deleteBookButton.classList.add("deleteBookButton");
+        markAsReadButton.addEventListener("click", function (e) {
+            title = e.target.parentNode.parentNode.querySelector(".bookTitle").textContent;
+            targetBook = library.getBook(title);
+            targetBook.toggleStatus();
+        
+            if (targetBook.status == true) {
+                e.target.parentNode.parentNode.querySelector(".bookStatus").textContent = "Read";
+            } 
+            if (targetBook.status == false) {
+                e.target.parentNode.parentNode.querySelector(".bookStatus").textContent = "Not Read";
+            }
+        })
 
         bookContent.append(bookTitle);
         bookContent.append(bookAuthor);
@@ -82,80 +111,69 @@ function updateBookDisplay(library) {
         buttonGroup.append(markAsReadButton);
         buttonGroup.append(deleteBookButton);
 
-        book.append(bookContent);
-        book.append(buttonGroup);
-        bookList.append(book);
-    }
-
-    const deleteBookButtons = document.querySelectorAll(".deleteBookButton");
-    const markAsReadButtons = document.querySelectorAll(".markAsReadButton");
-    deleteBookButtons.forEach(deleteBookButton => {
-        deleteBookButton.addEventListener("click", (e) => {
-            targetBookTitle = e.target.parentNode.parentNode.querySelector(".bookTitle").textContent;
-            library = library.filter( book => book.title !== targetBookTitle);
-            refreshBookDisplay();
-            updateBookDisplay(library);
-        })
-    })
-    
-    markAsReadButtons.forEach(markAsReadButton => {
-        markAsReadButton.addEventListener("click", function (e) {
-                console.log(`Mark as read button pressed`);
-                targetBookTitle = e.target.parentNode.parentNode.querySelector(".bookTitle").textContent;
-                targetBook = library.find(book => book.title == targetBookTitle);
-                targetBook.toggleStatus();
-    
-                if (targetBook.status == true) {
-                    e.target.parentNode.parentNode.querySelector(".bookStatus").textContent = "Read";
-                    console.log(`updating to ${e.target.parentNode.parentNode.querySelector(".bookStatus").textContent}`);
-                } 
-                
-                if (targetBook.status == false) {
-                    e.target.parentNode.parentNode.querySelector(".bookStatus").textContent = "Not Read";
-                    console.log(`updating to ${e.target.parentNode.parentNode.querySelector(".bookStatus").textContent}`);
-                }
-    
-    
-            })
-    })
+        bookCard.append(bookContent);
+        bookCard.append(buttonGroup);
+        bookList.append(bookCard);
+    }  
 
 
-}
+    var saveBookButton = document.querySelector(".saveBookButton");
+    var bookTitle = document.querySelector("#bookTitle");
+    var bookAuthor = document.querySelector("#bookAuthor");
+    var bookPages = document.querySelector("#bookPages");
+    var bookStatus = document.querySelector("#bookStatus");
 
-function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus) {
-    
-    let bookId = library.length;
 
-    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus, bookId);
-    console.log(`${bookTitle} by ${bookAuthor}, ${bookPages} pages, ${bookStatus}`)
-    library.push(newBook);
-    refreshBookDisplay();
-    updateBookDisplay(library);
-}
-
-const addBookButton = document.querySelector(".addBookButton");
-var addBookModal = document.querySelector("#addBookModal");
-var cancelModalButtons = document.querySelectorAll(".cancelModalButton");
-
-addBookButton.addEventListener("click", () => {
-    addBookModal.style.display = "block";
-})
-
-cancelModalButtons.forEach(cancelModalButton => {
-    cancelModalButton.addEventListener("click", () => {
+    saveBookButton.addEventListener("click", () => {
+        addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.checked);
         addBookModal.style.display = "none";
     })
-})
 
-var saveBookButton = document.querySelector(".saveBookButton");
-var bookTitle = document.querySelector("#bookTitle");
-var bookAuthor = document.querySelector("#bookAuthor");
-var bookPages = document.querySelector("#bookPages");
-var bookStatus = document.querySelector("#bookStatus");
+    function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus) {
+        
+        let bookId = library.length;
+
+        const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus, bookId);
+        library.addBook(newBook);
+        displayController.updateBooksDisplay();
+    }
+
+    const addBookButton = document.querySelector(".addBookButton");
+    var addBookModal = document.querySelector("#addBookModal");
+    var cancelModalButtons = document.querySelectorAll(".cancelModalButton");
+
+    addBookButton.addEventListener("click", () => {
+        addBookModal.style.display = "block";
+    })
+
+    cancelModalButtons.forEach(cancelModalButton => {
+        cancelModalButton.addEventListener("click", () => {
+            addBookModal.style.display = "none";
+        })
+    })
+
+    return { updateBooksDisplay }
+
+})();
 
 
-saveBookButton.addEventListener("click", () => {
-    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.checked);
-    addBookModal.style.display = "none";
-})
+const library = new Library();
+
+const book1 = new Book("Daring Greatly", "Brene Brown", 320, true, 0);
+const book2 = new Book("Bridge to Terabithia", "Katherine Paterson", 208, false, 1);
+const book3 = new Book("Animal Farm", "Goerge Orwell", 176, false, 2);
+
+library.addBook(book1);
+library.addBook(book2);
+library.addBook(book3);
+
+displayController.updateBooksDisplay();
+
+
+
+    
+    
+
+
+
 
